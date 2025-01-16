1da3a67278b8b9a596381c740d4ff7af387f9a59
ninjakaib <kai.breeze@me.com>
2025-01-15 18:17:57 -0800

    imports and legacy updates

--- hardware_accelerators/__init__.py
+++ hardware_accelerators/__init__.py
@@ -1,7 +1,13 @@
 from .dtypes import Float8, BF16
-from .rtllib.adders import float_adder, FloatAdderPipelined
-from .rtllib.multipliers import float_multiplier, FloatMultiplierPipelined
-from .rtllib.lmul import lmul_simple, lmul_fast, LmulPipelined
+from .rtllib import (
+    float_adder,
+    FloatAdderPipelined,
+    float_multiplier,
+    FloatMultiplierPipelined,
+    lmul_simple,
+    lmul_fast,
+    LmulPipelined,
+)
 
 __all__ = [
     "Float8",
--- hardware_accelerators/rtllib/__init__.py
+++ hardware_accelerators/rtllib/__init__.py
@@ -1,3 +1,13 @@
 from .adders import float_adder, FloatAdderPipelined
 from .multipliers import float_multiplier, FloatMultiplierPipelined
 from .lmul import lmul_simple, lmul_fast, LmulPipelined
+
+all = [
+    "float_adder",
+    "FloatAdderPipelined",
+    "float_multiplier",
+    "FloatMultiplierPipelined",
+    "lmul_simple",
+    "lmul_fast",
+    "LmulPipelined",
+]
