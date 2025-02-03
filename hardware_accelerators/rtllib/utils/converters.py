b893c181b3c119453becf0875b427a26eba1f20f
ninjakaib <kai.breeze@me.com>
2025-02-03 04:04:55 +0000

    please black please work

--- hardware_accelerators/rtllib/utils/converters.py
+++ hardware_accelerators/rtllib/utils/converters.py
@@ -1,8 +1,8 @@
 """Hardware module for converting between different floating point formats
     
-    Handles:
-    - Upcast/downcast between different float formats
-    - Mantissa alignment and rounding
-    - Exponent bias adjustment
-    - Special case handling (inf, nan, subnormals)
+Handles:
+- Upcast/downcast between different float formats
+- Mantissa alignment and rounding
+- Exponent bias adjustment
+- Special case handling (inf, nan, subnormals)
 """
