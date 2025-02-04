878c36ea997bfeacc03ad252121eb9dfd145463d
justin <87676813+nakschou@users.noreply.github.com>
2025-02-03 17:44:59 -0800

    Update next.config.mjs
    Signed-off-by: justin <87676813+nakschou@users.noreply.github.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export", // Enable static exports
-  basePath: "/hardware-accelerators-site", // Replace with your repo name
+  basePath: "/hardware-accelerators", // Replace with your repo name
   images: {
     unoptimized: true,
   },
