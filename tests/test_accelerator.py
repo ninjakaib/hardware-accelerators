4e84668e5bc9b335d97793395bae535da2ce7ff7
ninjakaib <kai.breeze@me.com>
2025-02-16 23:20:06 +0000

    new accelerator basic test

--- tests/test_accelerator.py
+++ tests/test_accelerator.py
@@ -3,8 +3,53 @@ from hardware_accelerators.rtllib import TiledAcceleratorConfig
 from hardware_accelerators.simulation import TiledMatrixEngineSimulator
 import numpy as np
 
+from hardware_accelerators.simulation.accelerator import AcceleratorSimulator
 
-def test_accelerator_matmul():
+
+def test_accelerator_basic():
+    simulator = AcceleratorSimulator.default_config(array_size=3, num_weight_tiles=2)
+
+    simulator.setup()
+
+    weights = np.ones((3, 3))
+    activations = np.array([[1, 2, 3], [-4, -5, -6], [7, 8, 9]])
+
+    simulator.load_weights(weights, 0)
+
+    simulator.execute_instruction(
+        data_vec=activations[0],
+        load_new_weights=True,
+        flush_pipeline=False,
+        activation_enable=True,
+        activation_func="relu",
+    )
+    simulator.execute_instruction(
+        data_vec=activations[1],
+        accum_addr=1,
+        flush_pipeline=False,
+        activation_enable=True,
+        activation_func="relu",
+    )
+    simulator.execute_instruction(
+        data_vec=activations[2],
+        accum_addr=2,
+        activation_enable=True,
+        activation_func="relu",
+        flush_pipeline=True,
+    )
+
+    results = np.zeros((activations.shape[0], weights.shape[1]))
+
+    for i in range(3):
+        results[i] = simulator._get_outputs()
+        simulator.execute_instruction(nop=True)
+
+    gt = np.maximum(0, (activations @ weights))
+
+    assert np.isclose(results, gt).all()
+
+
+def test_matrix_engine_matmul():
     """Test the matmul function."""
     config = TiledAcceleratorConfig(
         array_size=3,
