b6f608db76ad7d936f430933a9e706d30f645ccb
katemae <kate.abille@gmail.com>
2025-02-22 16:45:48 -0800

    fp16 test cases; SpecialValues failing due to lack of . in string

--- hardware_accelerators/dtypes/__init__.py
+++ hardware_accelerators/dtypes/__init__.py
@@ -2,5 +2,6 @@
 from .base import BaseFloat
 from .bfloat16 import BF16
 from .float8 import Float8
+from .float16 import Float16
 
-__all__ = ["BaseFloat", "Float8", "BF16"]
+__all__ = ["BaseFloat", "Float8", "BF16", "Float16"]
