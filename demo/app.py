8df2288c03bf0ab54e2977bb32fd40d8b9feee55
ninjakaib <kai.breeze@me.com>
2025-03-02 11:42:20 -0800

    incorporating new modules for demo

--- demo/app.py
+++ demo/app.py
@@ -12,7 +12,7 @@ import tqdm
 import pyrtl
 
 sys.path.append(".")
-from hardware_accelerators.nn.util import softmax
+from hardware_accelerators.nn.util import load_model, softmax
 from hardware_accelerators.simulation.matrix_utils import (
     bias_trick,
     count_total_gemv_tiles,
@@ -24,23 +24,19 @@ from hardware_accelerators.dtypes.bfloat16 import BF16
 from hardware_accelerators.dtypes.float8 import Float8
 from hardware_accelerators.nn import model_factory, get_pytorch_device
 from hardware_accelerators.rtllib import (
+    CompiledAcceleratorConfig,
     AcceleratorConfig,
     Accelerator,
     lmul_fast,
     float_multiplier,
 )
-from hardware_accelerators.simulation.compile import CompiledAcceleratorSimulator
-from hardware_accelerators.simulation import CompiledSimulator, AcceleratorSimulator
+from hardware_accelerators.simulation import CompiledAcceleratorSimulator
 
 
 # ------------ CONSTANTS ------------ #
 
 # Load the trained model
-model_path = "models/mlp_mnist.pth"
-model = model_factory()
-model.load_state_dict(
-    torch.load(model_path, map_location=get_pytorch_device(), weights_only=True)
-)
+model = load_model("models/mlp_mnist.pth")
 model.eval()
 
 classes = [
@@ -104,99 +100,37 @@ def torch_predict(sketchpad: EditorValue):
 
 
 def update_accelerator_config(
-    activations_dtype: str, weights_dtype: str, size: int, multiplication: str
-) -> AcceleratorConfig:
+    activations_dtype: str,
+    weights_dtype: str,
+    size: int,
+    multiplication: str,
+) -> CompiledAcceleratorConfig:
+    config = CompiledAcceleratorConfig(
+        array_size=size,
+        activation_type=BF16,
+        weight_type=BF16,
+        multiplier=float_multiplier,
+    )
 
     # Triggered by run simulation button
     print("update_accelerator_config fn called")
-    print(activations_dtype, weights_dtype, size, multiplication)
+    print(config)
 
-    return AcceleratorConfig(
-        num_weight_tiles=4,
-        weight_type=dtype_map[weights_dtype],
-        data_type=dtype_map[activations_dtype],
-        array_size=size,
-        pe_multiplier=mult_map[multiplication],
-        pe_adder=float_adder,
-        accum_adder=float_adder,
-        accum_addr_width=8,
-        accum_type=dtype_map[activations_dtype],
-        pipeline=False,
-    )
+    return config
 
 
 def sim_predict_progress(
     sketchpad: EditorValue,
-    config: AcceleratorConfig,
+    config: CompiledAcceleratorConfig,
     gr_progress=gr.Progress(track_tqdm=True),
 ):
     pyrtl.reset_working_block()
-    simulator = CompiledSimulator(config=config)
+    simulator = CompiledAcceleratorSimulator(config)
     chunk_size = config.array_size
 
     x = image_to_tensor(sketchpad).detach().numpy().flatten()
-    probabilities = simulator.run_mlp(model, x)
+    probabilities = simulator.predict(x)
     return {cls: float(prob) for cls, prob in zip(classes, probabilities)}
-
-    weights_1 = model.fc1.weight.numpy(force=True)
-    bias_1 = model.fc1.bias.numpy(force=True)
-    weights_2 = model.fc2.weight.numpy(force=True)
-    bias_2 = model.fc2.bias.numpy(force=True)
-
-    # Add bias to first layer weights and 1 to activations
-    W_aug, x_aug = bias_trick(weights_1, bias_1, x)
-
-    total_tiles = count_total_gemv_tiles([(784, 128), (128, 10)], chunk_size)
-    progress = tqdm.tqdm(total=total_tiles)
-
-    tile_generator = generate_gemv_tiles(x_aug, W_aug, chunk_size)
-
-    for tile in tile_generator:
-        simulator.load_weights(weights=tile.matrix.T, tile_addr=0)
-        simulator.execute_instruction(
-            load_new_weights=True,
-            weight_tile_addr=0,
-            data_vec=tile.vector,
-            accum_addr=tile.index,
-            accum_mode=not tile.first,
-            activation_func="relu",
-            activation_enable=tile.last,
-            flush_pipeline=True,
-        )
-        progress.update()
-
-    simulator.execute_instruction(nop=True)
-    simulator.execute_instruction(nop=True)
-
-    sim_fc1 = np.array(simulator.output_trace)
-
-    # simulator.reset_output_trace()
-    simulator.output_trace = []
-
-    W2_aug, fc1_aug = bias_trick(weights_2, bias_2, sim_fc1.flatten())
-
-    fc2_tile_generator = generate_gemv_tiles(fc1_aug, W2_aug, chunk_size)
-
-    for tile in fc2_tile_generator:
-        simulator.load_weights(weights=tile.matrix.T, tile_addr=0)
-        simulator.execute_instruction(
-            load_new_weights=True,
-            weight_tile_addr=0,
-            data_vec=tile.vector,
-            accum_addr=tile.index,
-            accum_mode=not tile.first,
-            activation_enable=tile.last,
-            flush_pipeline=True,
-        )
-        progress.update()
-
-    simulator.execute_instruction(nop=True)
-    simulator.execute_instruction(nop=True)
-
-    sim_fc2 = np.array(simulator.output_trace).flatten()
-    probabilities = softmax(sim_fc2)
-    result = {cls: float(prob) for cls, prob in zip(classes, probabilities)}
-    return result
 
 
 # ------------ Blocks UI Layout ------------ #
